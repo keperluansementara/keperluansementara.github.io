51f2beb0b018e88fe427e343894570e087bded25
keperluansementara <keperluansementara@gmail.com>
2026-03-18 02:51:10 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
   base: './', // ← INI WAJIB
+=======
+  base: '/', // ← ini penting banget
+>>>>>>> 5b63d0d (update)
 })
